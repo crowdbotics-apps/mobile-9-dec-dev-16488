8b96b609a690100472a8d5462b444f76be146326
Crowdbotics <team@crowdbotics.com>
2020-12-09 11:06:10 +0000

    Update models

--- backend/home/migrations/0004_auto_20201209_0554.py
+++ backend/home/migrations/0004_auto_20201209_0554.py
@@ -6,28 +6,28 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_users'),
+        ("home", "0003_users"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='users',
-            name='created_at',
+            model_name="users",
+            name="created_at",
         ),
         migrations.RemoveField(
-            model_name='users',
-            name='email',
+            model_name="users",
+            name="email",
         ),
         migrations.RemoveField(
-            model_name='users',
-            name='password',
+            model_name="users",
+            name="password",
         ),
         migrations.RemoveField(
-            model_name='users',
-            name='password_hash',
+            model_name="users",
+            name="password_hash",
         ),
         migrations.RemoveField(
-            model_name='users',
-            name='updated_at',
+            model_name="users",
+            name="updated_at",
         ),
     ]
--- backend/home/models.py
+++ backend/home/models.py
@@ -55,9 +55,85 @@ class CustomText(models.Model):
     def field(self):
         return "title"
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
 
 class HomePage(models.Model):
     body = models.TextField()
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
 
     @property
     def api(self):
@@ -96,4 +172,8 @@ class Users(models.Model):
     "Generated Model"
     name = models.CharField(
         max_length=100,
+    )
+    jhgjhghj = models.BigIntegerField(
+        null=True,
+        blank=True,
     )
