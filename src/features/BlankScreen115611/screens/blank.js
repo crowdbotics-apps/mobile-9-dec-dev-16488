28c2eba71ed5a20ad1f98c45e97da91233292fd0
Crowdbotics <team@crowdbotics.com>
2020-12-09 12:06:06 +0000

    Update components

--- src/features/BlankScreen115611/screens/blank.js
+++ src/features/BlankScreen115611/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,64 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <View style={styles.View_11}>
+        <View>
+          <Button title="Press me!" onPress={() => alert("Pressed!")} />
+        </View>
+        <View>
+          <Button title="Press me!" onPress={() => alert("Pressed!")} />
+        </View>
+      </View>
+      <View>
+        <View>
+          <Text>Sample text content</Text>
+          <Button title="Press me!" onPress={() => alert("Pressed!")} />
+        </View>
+        <View>
+          <Text>Sample text content</Text>
+          <Button title="Press me!" onPress={() => alert("Pressed!")} />
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  Button_3: {},
+  Button_5: {},
+  Button_7: {},
+  View_11: { flexDirection: "row" },
+  View_12: {},
+  Button_16: {},
+  View_13: {},
+  Button_19: {},
+  View_23: {},
+  View_24: {},
+  Text_80: {},
+  Button_29: {},
+  View_25: {},
+  Text_65: {},
+  Button_32: {}
+})
