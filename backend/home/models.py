a0838873b08b3b402884eeb3c7454078ba050867
Crowdbotics <team@crowdbotics.com>
2020-12-09 05:54:31 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -44,6 +44,17 @@ class CustomText(models.Model):
     def field(self):
         return "title"
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def api(self):
+        return f"/api/v1/customtext/{self.id}/"
+
+    @property
+    def field(self):
+        return "title"
+
 
 class HomePage(models.Model):
     body = models.TextField()
@@ -72,24 +83,17 @@ class HomePage(models.Model):
     def field(self):
         return "body"
 
+    @property
+    def api(self):
+        return f"/api/v1/homepage/{self.id}/"
+
+    @property
+    def field(self):
+        return "body"
+
 
 class Users(models.Model):
     "Generated Model"
-    email = models.EmailField(
-        max_length=254,
-    )
     name = models.CharField(
         max_length=100,
-    )
-    password = models.CharField(
-        max_length=100,
-    )
-    password_hash = models.CharField(
-        max_length=100,
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True,
     )
